fa646b1a2c3d89ba09180fe542e57acae22daae1
kolorobot <rafal.borowiec@gmail.com>
2015-02-08 19:11:23 +0100

    !

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -2,9 +2,17 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+
+    @RequestMapping(value = {"", "/"})
+    public String index() {
+        return "Running...";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
